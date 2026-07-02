44343f6b49d68527d266ce83815cd91f15f1d016
Prakhar Jain <88723250+Prakharjain1211@users.noreply.github.com>
2026-07-02 11:25:53 +0530

    Time: 26 ms (21.60%) | Memory: 13.3 MB (98.87%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.py
+++ 42-trapping-rain-water/trapping-rain-water.py
@@ -5,16 +5,16 @@ class Solution(object):
         :rtype: int
         """
         n = len(height)
-        Lmax = [0]*n
-        Rmax = [0]*n
-        ans = 0
-        Lmax[0] = height[0]
-        Rmax[n-1] = height[n-1]
+        ans,l,r,lmax,rmax=0,0,n-1,0,0
 
-        for i in range(1,n):
-            Lmax[i] = max(Lmax[i-1],height[i])
-        for i in range(n - 2, -1, -1):
-            Rmax[i] = max(Rmax[i+1],height[i])
-        for i in range(n):
-            ans+= min(Lmax[i],Rmax[i])-height[i]
+        while l<r:
+            lmax = max(lmax,height[l])
+            rmax = max(rmax,height[r])
+
+            if lmax < rmax:
+                ans+= lmax-height[l]
+                l+=1
+            else:
+                ans+= rmax-height[r]
+                r-=1
         return ans
